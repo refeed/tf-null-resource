8158b1513c8e932a0bfe0ddf2570531bbf44404c
Rafid Aslam <rafidteam@gmail.com>
2025-03-11 19:43:38 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+variable "resource_count" {
+  default = 1
+}
+
+variable "wait_time" {
+  default = 10
+}
+
+resource "null_resource" "hello_script" {
+  count = var.resource_count
+
+  triggers = {
+    timestamp = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo 'Hello, World!'
+      sleep ${var.wait_time}
+    EOT
+  }
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+output "message_lengths" {
+  value = [for i in range(var.resource_count): length("Hello, World!")]
+}
