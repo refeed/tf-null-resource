5334589f2db529acc848c429daa1524e944631df
Rafid Aslam <rafidteam@gmail.com>
2025-03-11 19:49:29 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,33 @@
-variable "resource_count" {
-  default = 1
+provider "aws" {
+  region = "us-east-1"  # Change this to your preferred AWS region
 }
 
-variable "wait_time" {
-  default = 10
-}
-
-resource "null_resource" "hello_script" {
-  count = var.resource_count
-
-  triggers = {
-    timestamp = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo 'Hello, World!'
-      sleep ${var.wait_time}
-    EOT
-  }
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"  # Change this to a unique bucket name
 
   tags = {
-    Name = "MyVPC"
+    Name        = "My S3 Bucket"
+    Environment = "Dev"
   }
 }
 
-output "message_lengths" {
-  value = [for i in range(var.resource_count): length("Hello, World!")]
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"  # Change this as needed ("public-read", "private", etc.)
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
